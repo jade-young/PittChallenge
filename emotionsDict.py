2d28dca91ef350956c8374d47db5f7271bf83abd
devonsmyth <57301272+devonsmyth@users.noreply.github.com>
2020-10-23 22:02:30 -0400

    Add files via upload

---
+++ emotionsDict.py
@@ -0,0 +1,19 @@
+emotionsDict = {sad: ['sad', 'depressed', 'upset', 'desperate', 'angry', 'disgusted', 'frustrated',
+                  'jealous','betrayed', 'abused', 'rejected'],
+            stressed: ['stressed', 'panic', 'uncomfortable', 'unsure','stuck','desperate','frozen','hurt'],
+            happy: ['happy', 'great', 'amused', 'excited', 'glad','delighted','pleased','charmed',
+                    'grateful','optimistic','content','joyful','loving','loved','secure','strong','brave',
+                    'prepared','relaxed','comforted'],
+            tired: ['bored', 'depressed', 'drained', 'tired','dejected', 'dull', 'fatigued', 'burn', 'burned', 'burnt'],
+            energized: ['strong', 'strengthened', 'motivated', 'focused', 'invigorated', 'determined', 'energized',
+                        'inspired', 'great']
+            }
+
+emotions = ['strong', 'strengthened', 'motivated', 'focused', 'invigorated', 'determined', 'energized', 'inspired',
+            'great', 'bored', 'depressed', 'drained', 'tired','dejected', 'dull', 'fatigued', 'burn', 'burned', 'burnt',
+            'happy', 'great', 'amused', 'excited', 'glad', 'delighted', 'pleased', 'charmed', 'grateful', 'optimistic',
+            'content', 'joyful', 'loving', 'loved', 'secure', 'strong', 'brave', 'prepared', 'relaxed', 'comforted',
+            'stressed', 'panic', 'uncomfortable', 'unsure','stuck','desperate','frozen','hurt','sad', 'depressed',
+            'upset', 'desperate', 'angry', 'disgusted', 'frustrated', 'jealous','betrayed', 'abused', 'rejected'
+            ]
+
