52f51e69d4c5d09e9c609c32e1c06ceb93718680
devonsmyth <57301272+devonsmyth@users.noreply.github.com>
2020-10-23 22:26:02 -0400

    Update emotionsDict.py

--- emotionsDict.py
+++ emotionsDict.py
@@ -1,19 +1,25 @@
-emotionsDict = {sad: ['sad', 'depressed', 'upset', 'desperate', 'angry', 'disgusted', 'frustrated',
-                  'jealous','betrayed', 'abused', 'rejected'],
-            stressed: ['stressed', 'panic', 'uncomfortable', 'unsure','stuck','desperate','frozen','hurt'],
-            happy: ['happy', 'great', 'amused', 'excited', 'glad','delighted','pleased','charmed',
+class Emotions:
+    def __init__(self):
+        self.emotions_dict = {'sad': ['sad', 'depressed', 'upset', 'desperate', 'angry', 'disgusted', 'frustrated',
+                  'jealous','betrayed', 'abused', 'rejected'], 'stressed': ['stressed', 'panic', 'uncomfortable', 'unsure','stuck','desperate','frozen','hurt'],
+                  'happy': ['happy', 'great', 'amused', 'excited', 'glad','delighted','pleased','charmed',
                     'grateful','optimistic','content','joyful','loving','loved','secure','strong','brave',
                     'prepared','relaxed','comforted'],
-            tired: ['bored', 'depressed', 'drained', 'tired','dejected', 'dull', 'fatigued', 'burn', 'burned', 'burnt'],
-            energized: ['strong', 'strengthened', 'motivated', 'focused', 'invigorated', 'determined', 'energized',
+            'tired': ['bored', 'depressed', 'drained', 'tired','dejected', 'dull', 'fatigued', 'burn', 'burned', 'burnt'],
+            'energized': ['strong', 'strengthened', 'motivated', 'focused', 'invigorated', 'determined', 'energized',
                         'inspired', 'great']
-            }
+        }
 
-emotions = ['strong', 'strengthened', 'motivated', 'focused', 'invigorated', 'determined', 'energized', 'inspired',
+        self.emotions_list = ['strong', 'strengthened', 'motivated', 'focused', 'invigorated', 'determined', 'energized', 'inspired',
             'great', 'bored', 'depressed', 'drained', 'tired','dejected', 'dull', 'fatigued', 'burn', 'burned', 'burnt',
             'happy', 'great', 'amused', 'excited', 'glad', 'delighted', 'pleased', 'charmed', 'grateful', 'optimistic',
             'content', 'joyful', 'loving', 'loved', 'secure', 'strong', 'brave', 'prepared', 'relaxed', 'comforted',
             'stressed', 'panic', 'uncomfortable', 'unsure','stuck','desperate','frozen','hurt','sad', 'depressed',
             'upset', 'desperate', 'angry', 'disgusted', 'frustrated', 'jealous','betrayed', 'abused', 'rejected'
             ]
-
+        # counters for the categories
+        self.sad = 0
+        self.stressed = 0
+        self.happy = 0
+        self.tired = 0
+        self.energized = 0
