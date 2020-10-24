aef339ed60ac5d294fd929fab8a98104506bf4a0
jamy68 <56887687+jamy68@users.noreply.github.com>
2020-10-24 12:57:02 -0400

    Update wesleywellness.py

--- wesleywellness.py
+++ wesleywellness.py
@@ -25,7 +25,7 @@ class Emotions:
                     'prepared','relaxed','comforted'],
             'tired': ['bored', 'depressed', 'drained', 'tired','dejected', 'dull', 'fatigued', 'burn', 'burned', 'burnt'],
             'energized': ['strong', 'strengthened', 'motivated', 'focused', 'invigorated', 'determined', 'energized',
-                        'inspired', 'great']
+                        'inspired']
         }
 
         self.emotions_list = ['strong', 'strengthened', 'motivated', 'focused', 'invigorated', 'determined', 'energized', 'inspired',
@@ -51,6 +51,7 @@ class InteractionLog:
         self.emotions = Emotions()
         self.reported_emotions = []
         self.symptoms = {}
+        self.interaction_number = 0
 
     def get_data(self):
         print("Good Morning, Devon!")
@@ -61,6 +62,7 @@ class InteractionLog:
 
     def log_data(self, new_data):
         # write to text file
+
         return True
 
     def interpret(self, data):
@@ -69,17 +71,67 @@ class InteractionLog:
                 if word.lower() == emotion.lower():
                     # add to reported_emotions
                     self.reported_emotions.append(word.lower())
+        print(self.reported_emotions)
+        for word in self.reported_emotions:
+            """for key in self.emotions.emotions_dict.keys():
+                for emotion in self.emotions.emotions_dict[key]:
+                    if word == emotion:
+                        self.emotions.key += 1"""
+            for emotion in self.emotions.emotions_dict['sad']:
+                if word == emotion:
+                    self.emotions.sad = self.emotions.sad + 1
+                    continue
+            for emotion in self.emotions.emotions_dict['stressed']:
+                if word == emotion:
+                    self.emotions.stressed = self.emotions.stressed + 1
+                    continue
+            for emotion in self.emotions.emotions_dict['happy']:
+                if word == emotion:
+                    self.emotions.happy = self.emotions.happy + 1
+                    continue
+            for emotion in self.emotions.emotions_dict['tired']:
+                if word == emotion:
+                    self.emotions.tired = self.emotions.tired + 1
+                    continue
+            for emotion in self.emotions.emotions_dict['energized']:
+                if word == emotion:
+                    self.emotions.energized += self.emotions.energized + 1
+        print("tallies for each category")
+        print("happy:", self.emotions.happy)
+        print("sad:", self.emotions.sad)
+        print("stressed:", self.emotions.stressed)
+        print("tired:", self.emotions.tired)
+        print("energized:", self.emotions.energized)
+        self.follow_up()
 
     def follow_up(self):
+        # find categories with most tallies (over time)
         patterns = []  # categories that have more than 3 tallies
-        for categories in self.emotions.emotion_dict.keys():
+        """for categories in self.emotions.emotions_dict.keys():
             # if category counter > 3 then append category to list
+            if self.emotions.emotions_dict.categories > 3:
+                patterns.append(categories)"""
+        if self.emotions.sad > 1:
+            patterns.append('sad')
+        if self.emotions.stressed > 1:
+            patterns.append('stressed')
+        if self.emotions.happy > 1:
+            patterns.append('happy')
+        if self.emotions.tired > 1:
+            patterns.append('tired')
+        if self.emotions.energized > 1:
+            patterns.append('energized')
+        print(patterns)
+
+        # find category with the most tallies (max) (single day)
+
 
 def main():
     wes = InteractionLog()
     data = wes.get_data()
-    wes.log_data(data)
+    # wes.log_data(data)
     wes.interpret(data)
+
 
 if __name__ == '__main__':
     main()
